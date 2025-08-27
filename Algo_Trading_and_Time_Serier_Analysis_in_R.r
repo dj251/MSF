a6ff877f0635646c0545b277b0c2034cbb048d3d
dj251 <dhruvjain2508@gmail.com~>
2025-08-26 19:23:52 -0700

    checkpoint

--- Algo_Trading_and_Time_Serier_Analysis_in_R.r
+++ Algo_Trading_and_Time_Serier_Analysis_in_R.r
@@ -1,5 +1,9 @@
 install.packages("quantmod")
 library(quantmod)
-getSymbols("AAPL", src = "yahoo", start = "2020-01-01", to = "2025-08-20")
+getSymbols("TSLA", src = "yahoo", start = "2020-01-01", to = "2025-08-20")
 AAPL$AAPL.Adjusted
 plot(Ad(AAPL)
+
+w = rnorm(100)
+w 
+acf(w)
